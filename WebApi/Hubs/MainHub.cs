331558f2a4a56cae0d5c3d29c4a5127925ef9980
Justin Vazquez <JustinRamon.Vazquez@gmail.com>
2019-02-13 11:37:34 +0100

    AccessTest User Anlegen

--- WebApi/Hubs/MainHub.cs
+++ WebApi/Hubs/MainHub.cs
@@ -1,7 +1,10 @@
-﻿using Core.Models;
+﻿using Core.Access;
+using Core.Helper;
+using Core.Models;
 using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,14 +18,38 @@ namespace WebApi.Hubs
             return true;
         }
 
-        public User Login(string name,string password)
+        public User Login(string user,string password)
         {
             User test = new User();
             test.ID = 1;
             test.Name = "Test";
             test.Email = "Test.Test@Test.de";
-           
+
+            var con = DbHelper.GetDbConnection();
+            con.Open();
+            if (UserAccess.GetName(con, user) == user)
+            {
+                var userID = UserAccess.GetIdByName(con, user);
+                var salt = UserAccess.GetSalt(con, userID);
+                var hash = UserAccess.GetHash(con, userID);
+                con.Close();
+
+                Debug.Write(SaltHashHelper.ValidatePassword(password, hash, salt));
+
+            }
+            else
+            {
+                con.Close();
+                Debug.Write(false);
+
+            }
+
             return test;
+        }
+
+        public void Anlegen(string name,string password)
+        {
+
         }
     }
 }
