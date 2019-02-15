ab5a5cacbd5c1b76942a088d9ca964d681c9a54d
Justin Vazquez <JustinRamon.Vazquez@gmail.com>
2019-02-15 14:02:52 +0100

    Status

---
+++ Core/Access/StatusAccess.cs
@@ -0,0 +1,23 @@
+﻿using Core.Helper;
+using Core.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Access
+{
+    public static class StatusAccess
+    {
+        public static List<StatusModel> GetAllFach()
+        {
+            var con = DbHelper.GetDbConnection();
+            var sql = $"Select * From SVS.Status";
+            var result = con.Query<StatusModel>(sql).AsList();
+            return result;
+        }
+
+
+      
+    }
+}
